bdb85438221ca2a9a000823249da9f9dad32974a
Iulia Chiriac <iulia.chiriac@yahoo.com>
2021-11-22 16:39:16 +0200

    Add exercises

---
+++ examples/script.py
@@ -0,0 +1,38 @@
+"""This is a docstring for script module"""
+
+print('Hello world!')
+
+print('Not a very'); print('good idea')
+
+command = (
+    "/Users/iulia/.virtualenvs/python-training-nov2021/bin/python "
+    "/Users/iulia/PycharmProjects/python-training-nov2021/examples/script.py"
+)
+print(command)
+
+long_variable_name_1 = 100
+long_variable_name_2 = 200
+long_variable_name_3 = 300
+long_variable_name_4 = 400
+long_variable_name_5 = 500
+
+result = (
+        long_variable_name_1 +
+        long_variable_name_2 +
+        long_variable_name_3 +
+        long_variable_name_4 +
+        long_variable_name_5
+)
+
+if long_variable_name_1 > 0:
+    print()
+
+digits_sum = 0
+min = 0  # shadowing
+
+sentence = """dir returns a list of all the attributes and methods of the object
+it receives as input. Attributes and methods of an object are available through
+the . operator: obj.attr or obj.method().
+"""
+
+print(sentence)
---
+++ examples/string_formatting.py
@@ -0,0 +1,25 @@
+name = 'george'
+age = 40
+
+print("My name is", name.capitalize(), "and my age next year is", age + 1)
+
+# %-formatting
+message = "My name is %s and my age next year is %d" % (name, age + 1)
+print(message)
+
+# str.format()
+message = "My name is {} and my age next year is {}".format(name, age + 1)
+print(message)
+
+message = "My name is {name} and my age next year is {age}".format(
+    name=name.capitalize(), age=age + 1)
+print(message)
+
+person = {'name': name.capitalize(), 'age': age + 1}
+
+message = "My name is {name} and my age next year is {age}".format(**person)
+print(message)
+
+# f-strings
+message = f'My name is {name.capitalize()} and my age next year is {age + 1}.'
+print(message)
