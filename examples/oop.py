9ecf753aecb11801fc046350275e0c18fc06854e
Iulia Chiriac <iulia.chiriac@yahoo.com>
2021-11-25 14:52:11 +0200

    Add OOP exercise

--- examples/oop.py
+++ examples/oop.py
@@ -1,29 +1,81 @@
+from datetime import date
+
+
 class Person:
     count = 0  # class variable
+    MIN_DATE_OF_BIRTH = date(1900, 1, 1)
 
-    def __init__(self, name, age=0):  # dunder / magic method
+    def __init__(self, name, date_of_birth=None):  # dunder / magic method
         self.name = name  # instance variables
-        self.age = age
-        Person.count += 1
+        self.date_of_birth = date_of_birth
+        self.increment_count()
 
-    def salute(self, greeting):
+    @property
+    def date_of_birth(self):
+        return self._date_of_birth
+
+    @date_of_birth.setter
+    def date_of_birth(self, value):
+        if value and value < self.MIN_DATE_OF_BIRTH:
+            raise ValueError('Invalid date of birth.')
+        self._date_of_birth = value
+
+    @property
+    def age(self):
+        if not self.date_of_birth:
+            raise ValueError('Cannot compute age.')
+        return self.compute_age(self.date_of_birth)
+
+    def salute(self, greeting):  # instance method
         print(f'{self.name} says "{greeting}!"')
+
+    @classmethod
+    def increment_count(cls):  # class method
+        cls.count += 1
+
+    @staticmethod
+    def compute_age(date_of_birth):
+        delta = date.today() - date_of_birth
+        return int(delta.days / 365.25)
+
+    def __str__(self):
+        return f'{self.__class__.__name__} object: name={self.name}'
+
+    def __int__(self):
+        return self.age
 
 
 if __name__ == '__main__':
-    anna = Person('Anna', 20)
+    anna = Person('Anna', date(1980, 1, 1))
+    print(anna)
+
+    try:
+        anna.date_of_birth = date(1800, 1, 1)
+    except ValueError as ex:
+        print(ex)
+
+    print(anna.date_of_birth)
+
     print(anna.name, anna.age)
     print(Person.count)
 
     john = Person('John')
-    print(john.name, john.age)
+    print(john.name)
     print(Person.count)
 
     john.height = 1.85
     print('John has height attribute:', hasattr(john, 'height'))
-    print(john.name, john.age, john.height)
+    print(john.name, john.height)
     del john.height
     print('John has height attribute:', hasattr(john, 'height'))
 
     anna.salute('Hi')
     john.salute('Good morning')
+
+    print(anna, john)
+    print(int(anna))
+
+    Person.salute(anna, 'Hello')
+    anna.salute('Hello')
+
+    print(Person.compute_age(date(1985, 2, 12)))
--- exercises/day4.md
+++ exercises/day4.md
@@ -19,3 +19,12 @@ Hint: `time.time()` function returns current time in seconds.
     * `bank name (str)`
     * `balance (int)`
 1. Create two methods in this class, one to withdraw money and another one to deposit money into the account. The withdraw method will not allow withdrawing more money than available and it will raise an exception when you attempt to do that.
+1. Create a class `Employee` with three instance attributes:
+    * `person name (str)`
+    * `bank account (BankAccount)`
+    * `salary (default 0) (int)`
+
+    1. Use a `property` for salary management. Salary should be set only on initialisation; you shouldn't be able to set the salary from outside the class.
+    1. Write a method `raise_salary` that receives a parameter `percent` that should be one of the following values: 5, 10, 20. Raise a `ValueError` if another value is received by this method.
+    The `raise_salary` method should raise the salary with 5%, 10% or 20%.
+    1. Create a method `receive_salary` that will deposit in the employee's bank account an amount equal to its salary.
